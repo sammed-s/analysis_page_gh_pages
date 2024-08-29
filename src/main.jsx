d82d9128f5783489588baef1950c202818bc8e3b
sammed-sankonatti <sammed.sankonatti18@gmail.com>
2024-08-29 21:20:48 +0530

    No page

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,11 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 import "./index.css";
 import { Provider } from "react-redux";
-import {store} from "./redux/store/store.js";
+import { store } from "./redux/store/store.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/drillbit-analysis">
       <Provider store={store}>
         <App />
       </Provider>
--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,12 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/analysis_page_gh_pages/",
+  base: process.env.VITE_BASE_URL_ANALYSIS || "/",
   plugins: [react()],
   server: {
     port: 3000,
+  },
+  build: {
+    outDir: "dist",
   },
 });
